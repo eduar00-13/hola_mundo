ba5baec21d65c969402babf52a6978b388923fee
Brandon Eduardo Jerónimo Morales <isc18350316@gmail.com>
2022-03-01 09:48:12 -0600

    Unico comit

---
+++ holamundo/src/App.jsx
@@ -0,0 +1,10 @@
+import Saludo from "./Saludo"
+function App() {
+  return (
+    <div className="App">
+    <Saludo />
+    </div>
+  );
+}
+
+export default App;
---
+++ holamundo/src/Saludo.jsx
@@ -0,0 +1,7 @@
+const Saludo = () => {
+    return (
+        <h1>Este es el segundo HOLA MUNDO  en ReactJS</h1>
+        );
+};
+ 
+export default Saludo;
